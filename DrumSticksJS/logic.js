4a2ec47fd3e56c31329075577c90476afbe993d5
Ram <ramkb466@gmail.com>
2020-07-25 19:39:12 -0400

    added css

--- DrumSticksJS/logic.js
+++ DrumSticksJS/logic.js
@@ -2,6 +2,7 @@ window.addEventListener('keydown', function(e){
  //console.log(e.keyCode);
  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   //  console.log(audio);
-  if(!audio) return;
+  if(!audio) return; // stop the function from running all together
+  audio.currentTime =0; // rewind to the start everytime they press
   audio.play();
 });
