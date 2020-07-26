713eac3fa7ecf1513be169389b82bc40610bdd92
Ram <ramkb466@gmail.com>
2020-07-25 20:22:43 -0400

    added the functionality to play sound

--- DrumSticksJS/logic.js
+++ DrumSticksJS/logic.js
@@ -1,8 +1,33 @@
-window.addEventListener('keydown', function(e){
- //console.log(e.keyCode);
- const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  //  console.log(audio);
-  if(!audio) return; // stop the function from running all together
-  audio.currentTime =0; // rewind to the start everytime they press
-  audio.play();
-});
+// window.addEventListener('keydown', function(e){
+//  //console.log(e.keyCode);
+//  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//   //  console.log(audio);
+//     const key = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//   if(!audio) return; // stop the function from running all together
+//   audio.currentTime =0; // rewind to the start everytime they presscom
+//   audio.play();
+//   key.classList.add('playing');
+//   //  key.classList.add('playing');
+
+// });
+
+function playSound(e) {
+    //console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    //  console.log(audio);
+    const key = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop the function from running all together
+    audio.currentTime = 0; // rewind to the start everytime they presscom
+    audio.play();
+    key.classList.add('playing');
+    //  key.classList.add('playing');
+
+}
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return; // skip if its not a transform
+    //console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown',playSound);
