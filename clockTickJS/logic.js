7ec06391a0138761ee6bd6eb50cb0e5ecec00500
Ram <ramkb466@gmail.com>
2020-07-27 17:18:15 -0400

    created logic of hands of clock

---
+++ clockTickJS/logic.js
@@ -0,0 +1,19 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegree = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    const min = now.getMinutes();
+    const minDegree = ((min / 60)* 360) + 90;
+    minHand.style.transform = `rotate(${minDegree}deg)`;
+
+    const hour = now.getHours();
+    const hourDegree = ((hour/12)*360) + 90;
+    hourHand.style.transform = `rotate(${hourHand}deg)`;
+}
+setInterval(setDate, 1000);
