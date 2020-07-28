413db8fc816cf90b25aa99269e5471f8300784a5
Ram <ramkb466@gmail.com>
2020-07-28 13:38:39 -0400

    changed the css variables using js

--- CssVariablesWithJS/index.js
+++ CssVariablesWithJS/index.js
@@ -6,7 +6,12 @@ function handleChange(){
     // console.log(this.dataset);
     const suffix = this.dataset.sizing || ''; // or empty string to check undefined because
     // we don't have the suffix like px for the color attribute.
-    console.log(suffix);
+    // console.log(suffix);
+
+    // now to select the css property of base, spacing and blur in js 
+    // we have to select the whole document ie root and set the attribute to base, spacing and blur
+    // we can do so becuase we have given name attribute to input elements with base, spacing and blur
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleChange));
