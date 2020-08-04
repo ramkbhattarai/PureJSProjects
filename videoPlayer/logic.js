d4705adcfb28dd69968987eef839cd7a6582303b
Ram <ramkb466@gmail.com>
2020-08-04 15:23:57 -0400

    functionality for playing and scrubing the video

--- videoPlayer/logic.js
+++ videoPlayer/logic.js
@@ -22,13 +22,35 @@ function handleSkip(){
 }
 
 function handleRangeUpdate(){
-    this.value;
+   video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `{percent}%`
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
 }
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', handleSkip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
+progress.addEventListener('mousedown', ()=> mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+
+
+
