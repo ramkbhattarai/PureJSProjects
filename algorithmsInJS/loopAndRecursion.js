6036ab9137521462e65f18e33039e1881809db6f
Ram <ramkb466@gmail.com>
2020-08-05 17:29:02 -0400

    wrapper and accumulating functions for recursion

--- algorithmsInJS/loopAndRecursion.js
+++ algorithmsInJS/loopAndRecursion.js
@@ -39,3 +39,58 @@ const factorialR = (n) => {
 // console.log(factorialR(3));
 
 
+// example 3
+const logNumbers = (start, end) => {
+    
+    for (let index = start; index <= end; index++) {
+        console.log(index);
+    }
+    return 'completed from loop';
+}
+
+// console.log(logNumbers(1,3));
+
+const logNumbersR = (start, end) => {
+    function recurse(i){
+        console.log(`recursively ${i}`);
+        if(i < end){
+            recurse(i+1);
+        }
+    }
+    recurse(start);
+}
+
+// logNumbersR(1,3);
+
+// two patterns for recursion
+//1. wrapper function
+// 2. accumulator
+
+
+// this is an example of wrapper function where we used recurse as the wrapper function
+const logNumbersR1 = (start, end) => {
+    function recurse(i) {
+        console.log(`recursively ${i}`);
+        if (i < end) {
+            recurse(i + 1);
+        }
+    }
+    recurse(start);
+}
+
+// this is an example of accumulator where we keep on adding the result and passing down to recursion
+// till it meets the base case;
+
+function joinElements(array, joinString){
+    function recurse1(index, resultSoFar){
+        resultSoFar += array[index];
+        // console.log(resultSoFar);
+        if(index === array.length -1){
+            return resultSoFar;
+        }else{
+            return recurse1(index + 1, resultSoFar+joinString);
+        }
+    }
+    return recurse1(0,'');
+}
+console.log(joinElements(['s','cr','t','cod',':) :)'], 'e'));
