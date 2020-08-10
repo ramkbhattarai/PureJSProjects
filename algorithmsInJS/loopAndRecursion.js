2b88b608b4b34d0b1235a6ea3aa36c33de48b497
Ram <ramkb466@gmail.com>
2020-08-09 22:42:05 -0400

    recursion with memoxiation

--- algorithmsInJS/loopAndRecursion.js
+++ algorithmsInJS/loopAndRecursion.js
@@ -263,3 +263,17 @@ function minChange(coins, amount, memo={}){
 }
 
 // console.log(minChange([1,10,25], 100));
+
+function change(amount, coins, memo ={}){
+    let key = amount + '-'+ coins;
+    if(key in memo) return memo[key];
+    if(amount === 0) return 1;
+    let currentCoin = coins[coins.length -1];
+    let total = 0;
+    for(let qty = 0; qty * currentCoin <= amount; qty++){
+        total += change(amount - qty * currentCoin, coins.slice(0, -1). memo);
+        
+    }
+    memo[key] = total;
+    return memo[key];
+}
