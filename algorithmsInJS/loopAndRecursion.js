4dfd0e62651d93a8a16a4ac3392b4fa0ccb83132
Ram <ramkb466@gmail.com>
2020-08-17 15:43:01 -0400

    fixed the bug

--- algorithmsInJS/loopAndRecursion.js
+++ algorithmsInJS/loopAndRecursion.js
@@ -379,21 +379,25 @@ function climbingStairs(n){
 function minimumPathSum(grid){
     let m = grid.length;
     let n = grid[0].length;
-    let table = new Array(m).fill().map(()=> new Array(n).fill(Infinity));
+    let table = new Array(m).fill().map(() => new Array(n).fill(Infinity));
     table[0][0] = grid[0][0];
-    for(let i = 0; i< m -1; i++){
-        for(let j = 0; j< n -1; j++){
-            table[i][j+1] = Math.min(
-                table[i][j] + grid[i][j+1]
-                ,
-                table[i][j + 1]
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (j < n - 1) {
+                table[i][j + 1] = Math.min(
+                    table[i][j] + grid[i][j + 1]
+                    ,
+                    table[i][j + 1]
                 );
-            table[i+1][j] = Math.min(
-                table[i][j] + grid[i+1][j]
-                ,
-                table[i+1][j]
-            );
+            }
+            if (i < m - 1) {
+                table[i + 1][j] = Math.min(
+                    table[i][j] + grid[i + 1][j]
+                    ,
+                    table[i + 1][j]
+                );
+            }
         }
     }
-    return table[m-1][n-1];
+    return table[m - 1][n - 1];
 }
