6485fc4f73522642b801cc24076f4c0a956035b5
Ram <ramkb466@gmail.com>
2020-08-20 20:47:41 -0400

    radix sort for both positive and negative numbers

--- effectivezSortingAlgorithms/radixSort.js
+++ effectivezSortingAlgorithms/radixSort.js
@@ -16,3 +16,32 @@ function radixSort(arr) {
     }
     return arr;
 }
+
+
+function radixSortWithNegatives(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    var negatives = arr.filter(item => item < 0);
+    var negativesSorted = [];
+    if (negatives.length > 0) {
+        negativesSorted = radixSort(negatives.map(item => Math.abs(item)))
+            .reverse()
+            .map(item => -item);
+    }
+
+    var positives = arr.filter(item => item >= 0);
+    let maxDigits = getMaxDigits(positives);
+
+    for (let k = 0; k < maxDigits; k++) {
+        let buckets = Array.from({ length: 10 }, () => []);
+
+        for (let i = 0; i < positives.length; i++) {
+            let digit = getDigitFrom(positives[i], k);
+            buckets[digit].push(positives[i]);
+        }
+        positives = [].concat(...buckets);
+    }
+    return negativesSorted.concat(positives);
+}
